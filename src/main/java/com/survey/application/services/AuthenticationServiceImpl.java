0d93324fe6a37dafd6e9d7a7f6e0ef477cfed2d0
Mateusz Dokowicz <mat.dok123@gmail.com>
2024-04-13 02:27:02 +0200

    correct syntax

--- src/main/java/com/survey/application/services/AuthenticationServiceImpl.java
+++ src/main/java/com/survey/application/services/AuthenticationServiceImpl.java
@@ -2,7 +2,7 @@ package com.survey.application.services;
 
 import com.survey.api.security.TokenProvider;
 import com.survey.application.dtos.CreateRespondentsAccountsDto;
-import com.survey.application.dtos.LoginDto;;
+import com.survey.application.dtos.LoginDto;
 import com.survey.domain.models.IdentityUser;
 import com.survey.domain.repository.IdentityUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 @Service
-public class AuthenticationServiceImpl implements AuthenticationService{
+public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final AuthenticationManager authenticationManager;
     private final TokenProvider tokenProvider;
@@ -28,7 +28,7 @@ public class AuthenticationServiceImpl implements AuthenticationService{
     @Autowired
     public AuthenticationServiceImpl(AuthenticationManager authenticationManager, TokenProvider tokenProvider,
                                      IdentityUserRepository identityUserRepository, PasswordEncoder passwordEncoder,
-                                     CredentialsGenerator credentialsGenerator){
+                                     CredentialsGenerator credentialsGenerator) {
         this.authenticationManager = authenticationManager;
         this.tokenProvider = tokenProvider;
         this.identityUserRepository = identityUserRepository;
