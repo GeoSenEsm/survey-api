38d93b084565873edb7a928bf76173635c15d901
Wiktoria Grzesik <wikgrz12@st.amu.edu.pl>
2024-05-28 07:41:09 +0200

    #44 create endpoint for getting surveys, that should be completed on a specific date

--- src/main/java/com/survey/api/controllers/SurveyController.java
+++ src/main/java/com/survey/api/controllers/SurveyController.java
@@ -4,13 +4,14 @@ import com.survey.application.dtos.surveyDtos.CreateSurveyDto;
 import com.survey.application.dtos.surveyDtos.ResponseSurveyDto;
 import com.survey.application.services.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/surveys")
@@ -27,5 +28,12 @@ public class SurveyController {
     public ResponseEntity<ResponseSurveyDto> createSurvey(@Validated @RequestBody CreateSurveyDto createSurveyDto){
         ResponseSurveyDto responseDto = surveyService.createSurvey(createSurveyDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseSurveyDto>> getSurveysByCompletionDate(
+            @RequestParam("completionDate") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate completionDate) {
+        List<ResponseSurveyDto> surveys = surveyService.getSurveysByCompletionDate(completionDate);
+        return ResponseEntity.ok(surveys);
     }
 }
--- src/main/java/com/survey/domain/repository/SurveyParticipationTimeSlotRepository.java
+++ src/main/java/com/survey/domain/repository/SurveyParticipationTimeSlotRepository.java
@@ -3,7 +3,11 @@ package com.survey.domain.repository;
 import com.survey.domain.models.SurveyParticipationTimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface SurveyParticipationTimeSlotRepository extends JpaRepository<SurveyParticipationTimeSlot, UUID> {
+    List<SurveyParticipationTimeSlot> findByFinishBetween(OffsetDateTime startOfDay, OffsetDateTime endOfDay);
 }
