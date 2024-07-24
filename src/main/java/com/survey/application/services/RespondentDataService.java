568d2eb2769f48a87ac3e631e934defdc0bcf69a
pawelFelcyn <89747833+pawelFelcyn@users.noreply.github.com>
2024-07-24 05:56:37 +0200

    #82: add endpoint that returns all respondents data (#38)

--- src/main/java/com/survey/application/services/RespondentDataService.java
+++ src/main/java/com/survey/application/services/RespondentDataService.java
@@ -6,7 +6,9 @@ import org.springframework.security.authentication.BadCredentialsException;
 
 import javax.management.InstanceAlreadyExistsException;
 import javax.management.InvalidAttributeValueException;
+import java.util.List;
 
 public interface RespondentDataService {
     RespondentDataDto createRespondent(CreateRespondentDataDto dto, String tokenBearerPrefix) throws BadCredentialsException, InvalidAttributeValueException, InstanceAlreadyExistsException;
+    List<RespondentDataDto> getAll();
 }
