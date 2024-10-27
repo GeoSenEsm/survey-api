32822764d3708af984d66e86822d956bbac1e71b
Wiktoria Grzesik <wikgrz12@st.amu.edu.pl>
2024-10-27 19:44:37 +0100

    add eager loading for options in InitialSurveyQuestion

--- src/main/java/com/survey/domain/models/InitialSurveyQuestion.java
+++ src/main/java/com/survey/domain/models/InitialSurveyQuestion.java
@@ -23,7 +23,7 @@ public class InitialSurveyQuestion {
     @Column(name = "content", nullable = false)
     private String content;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<InitialSurveyOption> options = new ArrayList<>();
 
     @ManyToOne
