b2dd27b776d3ec4ffd67ee7f7c4d72f1bfc9aa5d
Wiktoria Grzesik <wikgrz12@st.amu.edu.pl>
2024-11-09 21:58:52 +0100

    fix to #122

--- src/main/java/com/survey/application/services/SurveyValidationServiceImpl.java
+++ src/main/java/com/survey/application/services/SurveyValidationServiceImpl.java
@@ -45,10 +45,11 @@ public class SurveyValidationServiceImpl implements SurveyValidationService{
                         .filter(q -> q.getQuestionType().equals(QuestionType.image_choice.name()))
                         .mapToInt(q -> q.getOptions().size()))
                 .sum();
-
-        if (files.size() != imageChoiceOptionsCount) {
-            throw new IllegalArgumentException("Incorrect number of files uploaded for image choice questions. " +
-                    "Expected: " + imageChoiceOptionsCount + ", Found: " + files.size());
+        if(imageChoiceOptionsCount > 0) {
+            if (files.size() != imageChoiceOptionsCount) {
+                throw new IllegalArgumentException("Incorrect number of files uploaded for image choice questions. " +
+                        "Expected: " + imageChoiceOptionsCount + ", Found: " + files.size());
+            }
         }
     }
 
