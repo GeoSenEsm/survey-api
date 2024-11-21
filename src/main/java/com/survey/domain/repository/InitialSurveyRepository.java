3c71200127f9591d5cc628fd0855b39e378ed80b
Wiktoria Grzesik <wikgrz12@st.amu.edu.pl>
2024-11-21 20:14:01 +0100

    fix for getting the newest initial survey

--- src/main/java/com/survey/domain/repository/InitialSurveyRepository.java
+++ src/main/java/com/survey/domain/repository/InitialSurveyRepository.java
@@ -2,11 +2,13 @@ package com.survey.domain.repository;
 
 import com.survey.domain.models.InitialSurvey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface InitialSurveyRepository extends JpaRepository<InitialSurvey, UUID> {
-    Optional<InitialSurvey> findTopByOrderByIdAsc();
+    @Query(value = "SELECT TOP 1 * FROM initial_survey ORDER BY row_version DESC", nativeQuery = true)
+    Optional<InitialSurvey> findTopByRowVersionDesc();
 }
