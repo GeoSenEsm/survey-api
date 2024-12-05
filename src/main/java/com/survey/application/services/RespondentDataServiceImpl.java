59e9af2729fcec3c67cc8f85cc89bea267692303
Wiktoria Grzesik <wikgrz12@st.amu.edu.pl>
2024-12-05 21:05:19 +0100

    add filters to respondent

--- src/main/java/com/survey/application/services/RespondentDataServiceImpl.java
+++ src/main/java/com/survey/application/services/RespondentDataServiceImpl.java
@@ -2,14 +2,12 @@ package com.survey.application.services;
 
 import com.survey.application.dtos.CreateRespondentDataDto;
 import com.survey.domain.models.*;
+import com.survey.domain.models.enums.RespondentFilterOption;
+import com.survey.domain.models.enums.Visibility;
 import com.survey.domain.repository.*;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 import org.apache.coyote.BadRequestException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
@@ -18,6 +16,7 @@ import org.springframework.web.context.annotation.RequestScope;
 
 import javax.management.InstanceAlreadyExistsException;
 import javax.management.InvalidAttributeValueException;
+import java.time.OffsetDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -28,7 +27,6 @@ public class RespondentDataServiceImpl implements RespondentDataService{
     private final RespondentDataRepository respondentDataRepository;
     private final ClaimsPrincipalService claimsPrincipalService;
     private final EntityManager entityManager;
-    private final IdentityUserRepository identityUserRepository;
     private final InitialSurveyRepository initialSurveyRepository;
     private final InitialSurveyQuestionRepository initialSurveyQuestionRepository;
     private final InitialSurveyOptionRepository initialSurveyOptionRepository;
@@ -39,11 +37,10 @@ public class RespondentDataServiceImpl implements RespondentDataService{
 
     @Autowired
     public RespondentDataServiceImpl(RespondentDataRepository respondentDataRepository,
-                                     ClaimsPrincipalService claimsPrincipalService, IdentityUserRepository identityUserRepository, EntityManager entityManager, InitialSurveyRepository initialSurveyRepository, InitialSurveyQuestionRepository initialSurveyQuestionRepository, InitialSurveyOptionRepository initialSurveyOptionRepository, RespondentGroupRepository respondentGroupRepository, RespondentToGroupRepository respondentToGroupRepository) {
+                                     ClaimsPrincipalService claimsPrincipalService, EntityManager entityManager, InitialSurveyRepository initialSurveyRepository, InitialSurveyQuestionRepository initialSurveyQuestionRepository, InitialSurveyOptionRepository initialSurveyOptionRepository, RespondentGroupRepository respondentGroupRepository, RespondentToGroupRepository respondentToGroupRepository) {
         this.respondentDataRepository = respondentDataRepository;
         this.claimsPrincipalService = claimsPrincipalService;
         this.entityManager = entityManager;
-        this.identityUserRepository = identityUserRepository;
         this.initialSurveyRepository = initialSurveyRepository;
         this.initialSurveyQuestionRepository = initialSurveyQuestionRepository;
         this.initialSurveyOptionRepository = initialSurveyOptionRepository;
@@ -68,16 +65,126 @@ public class RespondentDataServiceImpl implements RespondentDataService{
 
     @Override
     @Transactional
-    public List<Map<String, Object>> getAll(){
+    public List<Map<String, Object>> getAll(RespondentFilterOption filterOption, Integer amount, OffsetDateTime from, OffsetDateTime to){
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<RespondentData> cq = cb.createQuery(RespondentData.class);
-        Root<RespondentData> respondentData = cq.from(RespondentData.class);
-        cq.select(respondentData);
+        CriteriaQuery<IdentityUser> cq = cb.createQuery(IdentityUser.class);
+        Root<IdentityUser> identityUserRoot = cq.from(IdentityUser.class);
+        List<Predicate> predicates = new ArrayList<>();
+        predicates.add(cb.equal(identityUserRoot.get("role"), "Respondent"));
+
+        if(filterOption != null){
+            if (from == null || to == null || amount == null) {
+                throw new IllegalArgumentException("'from', 'to' and 'amount' are required for filtering.");
+            }
+            switch (filterOption) {
+                case skipped_surveys:
+                    Subquery<UUID> activeSurveySubquery = cq.subquery(UUID.class);
+                    Root<SurveyParticipationTimeSlot> timeSlotRoot = activeSurveySubquery.from(SurveyParticipationTimeSlot.class);
+                    activeSurveySubquery.select(timeSlotRoot.get("surveySendingPolicy").get("survey").get("id"))
+                            .where(
+                                    cb.and(
+                                            cb.equal(timeSlotRoot.get("isDeleted"), false),
+                                            cb.lessThanOrEqualTo(timeSlotRoot.get("finish"), to),
+                                            cb.greaterThanOrEqualTo(timeSlotRoot.get("start"), from)
+                                    )
+                            );
+
+                    Subquery<UUID> visibleSurveySubquery = cq.subquery(UUID.class);
+                    Root<SurveySection> surveySectionRoot = visibleSurveySubquery.from(SurveySection.class);
+
+                    Predicate alwaysVisible = cb.equal(surveySectionRoot.get("visibility"), Visibility.always);
+
+                    Join<SurveySection, SectionToUserGroup> sectionToGroupJoin = surveySectionRoot.join("sectionToUserGroups", JoinType.LEFT);
+                    Join<SectionToUserGroup, RespondentGroup> groupJoin = sectionToGroupJoin.join("group", JoinType.LEFT);
+                    Join<RespondentGroup, RespondentData> respondentDataJoin = groupJoin.join("respondentData", JoinType.LEFT);
+
+                    Predicate groupSpecificVisible = cb.and(
+                            cb.equal(surveySectionRoot.get("visibility"), Visibility.group_specific),
+                            respondentDataJoin.get("identityUserId").in(identityUserRoot.get("id"))
+                    );
+
+                    Predicate sectionVisible = cb.or(alwaysVisible, groupSpecificVisible);
+
+                    visibleSurveySubquery.select(surveySectionRoot.get("survey").get("id"))
+                            .where(sectionVisible);
+
+                    Subquery<UUID> activeVisibleSurveysSubquery = cq.subquery(UUID.class);
+                    Root<SurveySection> activeVisibleRoot = activeVisibleSurveysSubquery.from(SurveySection.class);
+                    activeVisibleSurveysSubquery.select(activeVisibleRoot.get("survey").get("id"))
+                            .where(
+                                    cb.and(
+                                            activeVisibleRoot.get("survey").get("id").in(activeSurveySubquery),
+                                            activeVisibleRoot.get("survey").get("id").in(visibleSurveySubquery)
+                                    )
+                            );
+
+                    Subquery<Long> activeVisibleSurveysCountSubquery = cq.subquery(Long.class);
+                    Root<SurveySection> countRoot = activeVisibleSurveysCountSubquery.from(SurveySection.class);
+                    activeVisibleSurveysCountSubquery.select(cb.countDistinct(countRoot.get("survey").get("id")))
+                            .where(countRoot.get("survey").get("id").in(activeVisibleSurveysSubquery));
+
+                    Subquery<Long> participationCountSubquery = cq.subquery(Long.class);
+                    Root<SurveyParticipation> participationRoot = participationCountSubquery.from(SurveyParticipation.class);
+                    participationCountSubquery.select(cb.count(participationRoot.get("id")))
+                            .where(
+                                    cb.and(
+                                            cb.equal(participationRoot.get("identityUser").get("id"),
+                                                    identityUserRoot.get("id")),
+                                            participationRoot.get("survey").get("id").in(
+                                                    activeVisibleSurveysSubquery
+                                            )
+                                    )
+                            );
+
+                    predicates.add(cb.ge(
+                            cb.diff(
+                                    activeVisibleSurveysCountSubquery,
+                                    participationCountSubquery.getSelection()
+                            ),
+                            amount.longValue()
+                    ));
+                    break;
+                case location_not_sent:
+                    OffsetDateTime adjustedFrom = from.plusDays(1);
+                    OffsetDateTime adjustedTo = to.plusDays(1);
+
+                    Subquery<Long> localizationCountSubquery = cq.subquery(Long.class);
+                    Root<LocalizationData> localizationRoot = localizationCountSubquery.from(LocalizationData.class);
+
+                    localizationCountSubquery.select(cb.count(localizationRoot.get("id")))
+                            .where(
+                                    cb.and(
+                                            cb.equal(localizationRoot.get("identityUser").get("id"), identityUserRoot.get("id")),
+                                            cb.between(localizationRoot.get("dateTime"), adjustedFrom, adjustedTo)
+                                    )
+                            );
+
+                    predicates.add(cb.lessThanOrEqualTo(localizationCountSubquery.getSelection(), amount.longValue()));
+                    break;
+                case sensors_data_not_sent:
+                    Subquery<Long> sensorDataCountSubquery = cq.subquery(Long.class);
+                    Root<SensorData> sensorDataRoot = sensorDataCountSubquery.from(SensorData.class);
+
+                    sensorDataCountSubquery.select(cb.count(sensorDataRoot.get("id")))
+                            .where(
+                                    cb.equal(sensorDataRoot.get("respondent").get("id"), identityUserRoot.get("id")),
+                                    cb.between(sensorDataRoot.get("dateTime"), from, to)
+                            );
+
+                    predicates.add(cb.lessThanOrEqualTo(sensorDataCountSubquery.getSelection(), amount.longValue()));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid filter option: " + filterOption);
+
+            }
+        }
+
+        cq.select(identityUserRoot).where(cb.and(predicates.toArray(new Predicate[0])));
 
         return entityManager.createQuery(cq)
                 .getResultList()
                 .stream()
-                .map(this::mapRespondentDataToResponse)
+                .map(this::mapResultToResponse)
                 .collect(Collectors.toList());
     }
 
@@ -140,7 +247,6 @@ public class RespondentDataServiceImpl implements RespondentDataService{
         respondentToGroupRepository.saveAllAndFlush(respondentToGroupList);
     }
 
-
     private RespondentData initializeRespondentData(UUID userId) {
         RespondentData respondentData = new RespondentData();
         respondentData.setIdentityUserId(userId);
@@ -155,15 +261,6 @@ public class RespondentDataServiceImpl implements RespondentDataService{
                     InitialSurvey newSurvey = new InitialSurvey();
                     return initialSurveyRepository.save(newSurvey);
                 });
-    }
-
-    private UUID getCurrentUserUUID() {
-        String usernameFromJwt = getUsernameFromJwt();
-        return getUserUUID(usernameFromJwt);
-    }
-
-    private String getUsernameFromJwt() {
-        return claimsPrincipalService.getCurrentUsernameIfExists();
     }
 
     private void checkIfRespondentDataExists(UUID userId) throws InstanceAlreadyExistsException {
@@ -221,25 +318,36 @@ public class RespondentDataServiceImpl implements RespondentDataService{
         return respondentDataRepository.existsByIdentityUserId(userId);
     }
 
-    private UUID getUserUUID(String username) {
-        return identityUserRepository.findByUsername(username)
-                .map(IdentityUser::getId)
-                .orElse(null);
-    }
-
     private Map<String, Object> mapRespondentDataToResponse(RespondentData respondentData) {
         Map<String, Object> response = new LinkedHashMap<>();
         response.put("id", respondentData.getIdentityUser().getId());
         response.put("username", respondentData.getUsername());
 
-        respondentData.getRespondentDataQuestions().forEach(respondentDataQuestion -> {
-            String questionContent = respondentDataQuestion.getQuestion().getContent();
-            respondentDataQuestion.getOptions().forEach(respondentDataOption -> {
-                UUID optionId = respondentDataOption.getOption().getId();
-                response.put(questionContent, optionId);
+        mapQuestionsAndOptions(respondentData, response);
+   
+        return response;
+    }
+    private Map<String, Object> mapResultToResponse(IdentityUser identityUser) {
+        Map<String, Object> response = new LinkedHashMap<>();
+        UUID identityUserId = identityUser.getId();
+
+        response.put("id", identityUserId);
+        response.put("username", identityUser.getUsername());
+
+        RespondentData respondentData = respondentDataRepository.findByIdentityUserId(identityUserId);
+
+        if (respondentData != null) {
+            mapQuestionsAndOptions(respondentData, response);
+        }
+        return response;
+    }
+    private void mapQuestionsAndOptions(RespondentData respondentData, Map<String, Object> response) {
+        respondentData.getRespondentDataQuestions().forEach(question -> {
+            String questionContent = question.getQuestion().getContent();
+            question.getOptions().forEach(option -> {
+                String optionContent = option.getOption().getContent();
+                response.put(questionContent, optionContent);
             });
         });
-
-        return response;
     }
 }
