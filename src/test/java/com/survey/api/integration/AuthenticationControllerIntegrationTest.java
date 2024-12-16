cd5b457a89d1c87276a85745f7f2ad40506f4a0c
Wiktoria Grzesik <wikgrz12@st.amu.edu.pl>
2024-12-16 21:35:27 +0100

    add integration tests

--- src/test/java/com/survey/api/integration/AuthenticationControllerIntegrationTest.java
+++ src/test/java/com/survey/api/integration/AuthenticationControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import com.survey.api.TestUtils;
 import com.survey.api.security.Role;
 import com.survey.application.dtos.CreateRespondentsAccountsDto;
 import com.survey.application.dtos.LoginDto;
+import com.survey.application.dtos.surveyDtos.ChangePasswordDto;
 import com.survey.domain.models.IdentityUser;
 import com.survey.domain.repository.IdentityUserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +22,8 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @ExtendWith(IntegrationTestDatabaseInitializer.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = "ADMIN_USER_PASSWORD=testAdminPassword")
@@ -29,22 +32,24 @@ public class AuthenticationControllerIntegrationTest {
     private final WebTestClient webTestClient;
     private final ObjectMapper objectMapper;
     private final IdentityUserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final TestUtils testUtils;
+    private final PasswordEncoder passwordEncoder;
     private static final String ADMIN_PASSWORD = "testAdminPassword";
-    private static final String RESPONDENT_PASSWORD = "testRespondentPassword";
+    private static final String RESPONDENT_PASSWORD = "testRespondentPassword!";
+    private static final String RESPONDENT2_PASSWORD = "testRespondent2Password!";
+    private static final String NEW_VALID_RESPONDENT_PASSWORD = "!newTestRespondentPassword1";
+    private static final String NEW_INVALID_RESPONDENT_PASSWORD = "!newPassword";
 
 
     @Autowired
     public AuthenticationControllerIntegrationTest(WebTestClient webTestClient, ObjectMapper objectMapper,
-                                                   IdentityUserRepository identityUserRepository,
-                                                   PasswordEncoder passwordEncoder, TestUtils testUtils){
+                                                   IdentityUserRepository identityUserRepository, TestUtils testUtils, PasswordEncoder passwordEncoder){
 
         this.webTestClient = webTestClient;
         this.objectMapper = objectMapper;
         this.userRepository = identityUserRepository;
-        this.passwordEncoder = passwordEncoder;
         this.testUtils = testUtils;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @BeforeEach
@@ -128,7 +133,7 @@ public class AuthenticationControllerIntegrationTest {
     }
 
     @Test
-    public void testCreateRespondentsAccountsWithValidAdminCredentials() throws JsonProcessingException {
+    public void testCreateRespondentsAccountsWithValidAdminCredentials() {
         IdentityUser admin = testUtils.createUserWithRole(Role.ADMIN.getRoleName(), ADMIN_PASSWORD);
         String adminToken = testUtils.authenticateAndGenerateToken(admin, ADMIN_PASSWORD);
 
@@ -146,7 +151,7 @@ public class AuthenticationControllerIntegrationTest {
     }
 
     @Test
-    public void testCreateRespondentsAccountsWithInvalidCredentials() throws JsonProcessingException {
+    public void testCreateRespondentsAccountsWithInvalidCredentials() {
         CreateRespondentsAccountsDto dto = new CreateRespondentsAccountsDto(3);
 
         webTestClient.post().uri("/api/authentication/respondents")
@@ -211,5 +216,123 @@ public class AuthenticationControllerIntegrationTest {
                 .isBadRequest();
     }
 
+    @Test
+    void editRespondentPassword_ShouldReturnOk_WhenAdminUpdatesRespondentPassword() {
+        IdentityUser admin = testUtils.createUserWithRole(Role.ADMIN.getRoleName(), ADMIN_PASSWORD);
+        String adminToken = testUtils.authenticateAndGenerateToken(admin, ADMIN_PASSWORD);
+
+        IdentityUser respondent = testUtils.createUserWithRole(Role.RESPONDENT.getRoleName(), RESPONDENT_PASSWORD);
+
+        ChangePasswordDto changePasswordDto = createChangePasswordDto(null, NEW_VALID_RESPONDENT_PASSWORD);
+
+        webTestClient.patch()
+                .uri("api/authentication/" + respondent.getId() + "/password")
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(changePasswordDto)
+                .exchange()
+                .expectStatus().isOk();
+
+        assertPasswordChanged(respondent.getId(), NEW_VALID_RESPONDENT_PASSWORD);
+    }
+
+    @Test
+    void editRespondentPassword_ShouldReturnOk_WhenAdminUpdatesOwnPassword() {
+        IdentityUser admin = testUtils.createUserWithRole(Role.ADMIN.getRoleName(), ADMIN_PASSWORD);
+        String adminToken = testUtils.authenticateAndGenerateToken(admin, ADMIN_PASSWORD);
+
+        ChangePasswordDto changePasswordDto = createChangePasswordDto(ADMIN_PASSWORD, NEW_VALID_RESPONDENT_PASSWORD);
+
+        webTestClient.patch()
+                .uri("api/authentication/" + admin.getId() + "/password")
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(changePasswordDto)
+                .exchange()
+                .expectStatus().isOk();
+
+        assertPasswordChanged(admin.getId(), NEW_VALID_RESPONDENT_PASSWORD);
+    }
+
+    @Test
+    void updateRespondentPassword_ShouldReturnOk_WhenRespondentUpdatesOwnPassword() {
+        IdentityUser respondent = testUtils.createUserWithRole(Role.RESPONDENT.getRoleName(), RESPONDENT_PASSWORD);
+        String respondentToken = testUtils.authenticateAndGenerateToken(respondent, RESPONDENT_PASSWORD);
+
+        ChangePasswordDto changePasswordDto = createChangePasswordDto(RESPONDENT_PASSWORD, NEW_VALID_RESPONDENT_PASSWORD);
+
+        webTestClient.patch()
+                .uri("api/authentication/" + respondent.getId() + "/password")
+                .header("Authorization", "Bearer " + respondentToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(changePasswordDto)
+                .exchange()
+                .expectStatus().isOk();
+
+        assertPasswordChanged(respondent.getId(), NEW_VALID_RESPONDENT_PASSWORD);
+    }
+
+    @Test
+    void updateRespondentPassword_ShouldReturnBadRequest_WhenOldPasswordIsIncorrect() {
+        IdentityUser respondent = testUtils.createUserWithRole(Role.RESPONDENT.getRoleName(), RESPONDENT_PASSWORD);
+        String respondentToken = testUtils.authenticateAndGenerateToken(respondent, RESPONDENT_PASSWORD);
+
+        ChangePasswordDto changePasswordDto = createChangePasswordDto("invalid_old_password", NEW_VALID_RESPONDENT_PASSWORD);
+
+        webTestClient.patch()
+                .uri("api/authentication/" + respondent.getId() + "/password")
+                .header("Authorization", "Bearer " + respondentToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(changePasswordDto)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void updateRespondentPassword_ShouldReturnForbidden_WhenRespondentTriesToUpdateAnotherRespondentsPassword() {
+        IdentityUser respondent1 = testUtils.createUserWithRole(Role.RESPONDENT.getRoleName(), RESPONDENT_PASSWORD);
+        String respondent1Token = testUtils.authenticateAndGenerateToken(respondent1, RESPONDENT_PASSWORD);
+
+        IdentityUser respondent2 = testUtils.createUserWithRole(Role.RESPONDENT.getRoleName(), RESPONDENT2_PASSWORD);
+
+        ChangePasswordDto changePasswordDto = createChangePasswordDto(RESPONDENT2_PASSWORD, NEW_VALID_RESPONDENT_PASSWORD);
+
+        webTestClient.patch()
+                .uri("api/authentication/" + respondent2.getId() + "/password")
+                .header("Authorization", "Bearer " + respondent1Token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(changePasswordDto)
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+    @Test
+    void updateRespondentPassword_ShouldReturnBadRequest_WhenNewPasswordDoesNotMeetComplexityRequirements() {
+        IdentityUser respondent = testUtils.createUserWithRole(Role.RESPONDENT.getRoleName(), RESPONDENT_PASSWORD);
+        String respondentToken = testUtils.authenticateAndGenerateToken(respondent, RESPONDENT_PASSWORD);
+
+        ChangePasswordDto changePasswordDto = createChangePasswordDto(RESPONDENT_PASSWORD, NEW_INVALID_RESPONDENT_PASSWORD);
+
+        webTestClient.patch()
+                .uri("api/authentication/" + respondent.getId() + "/password")
+                .header("Authorization", "Bearer " + respondentToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(changePasswordDto)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    private void assertPasswordChanged(UUID userId, String expectedPassword) {
+        IdentityUser user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalStateException("User not found after password update"));
+
+        assertTrue(passwordEncoder.matches(expectedPassword, user.getPasswordHash()));
+    }
+    private ChangePasswordDto createChangePasswordDto(String oldPassword, String newPassword){
+        ChangePasswordDto changePasswordDto = new ChangePasswordDto();
+        changePasswordDto.setOldPassword(oldPassword);
+        changePasswordDto.setNewPassword(newPassword);
+        return changePasswordDto;
+    }
 
 }
