ca2effb3c0c15e9ffc678d5a6cd90f11bd81ab29
mawit <marcin.witkowski@rockwool.com>
2026-01-11 23:07:14 +0100

    Fix query perfomance issues for SensorData LocalizationData and SurveyData attempt 3

--- src/main/java/com/survey/application/services/SensorDataServiceImpl.java
+++ src/main/java/com/survey/application/services/SensorDataServiceImpl.java
@@ -12,9 +12,6 @@ import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
